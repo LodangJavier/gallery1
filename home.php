5c883dea71308637f87a84a64bd4022153e4585f
unknown <indahresmawati10@gmail.com>
2024-03-09 14:08:51 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,52 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['userid'])){
+        header("location:login.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+    <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+</style>
+</head>
+<body>
+    <h1>Halaman Home</h1>
+    <p>Selamat datang <b><?=$_SESSION['namalengkap']?></b></p>
+    
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="album.php">Album</a></li>
+        <li><a href="foto.php">Foto</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</body>
+</html>
